6dc5973d7964b1dfb78d7527d3892a610ff8b11c
francesco <francesco.pezzati@iia.cnr.it>
2017-05-15 16:44:35 +0200

    Having trouble about Balana docs. Basically there is no documentation. So sad :(

--- src/test/java/edu/pezzati/sec/xaml/SimplePDPTest.java
+++ src/test/java/edu/pezzati/sec/xaml/SimplePDPTest.java
@@ -27,11 +27,13 @@ import org.wso2.balana.xacml3.Attributes;
 public class SimplePDPTest {
 
     private final Logger log = Logger.getLogger(getClass());
+    private static final int PERMIT = 0;
+    //    private static final int DENY = 1;
+    //    private static final int INDETERMINATE = 2;
     private static final int NOT_APPLICABLE = 3;
     private PDPConfig pdpConfig;
     private PDP simplePDP;
     private File singlePdpConf;
-    private File singlePolicyMultipleRules;
     private File singlePolicySingleRule;
 
     @Before
@@ -39,8 +41,7 @@ public class SimplePDPTest {
 	System.clearProperty(ConfigurationStore.PDP_CONFIG_PROPERTY);
 	singlePdpConf = new File("src/test/resources/conf/simple.pdp.conf.xml");
 	System.clearProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY);
-	singlePolicyMultipleRules = new File("src/test/resources/policies");
-	singlePolicySingleRule = new File("src/test/resources/policies");
+	singlePolicySingleRule = new File("src/test/resources/singlepolicy");
     }
 
     public void generatePDPWithNullConfiguration() throws URISyntaxException {
@@ -48,13 +49,13 @@ public class SimplePDPTest {
 	Balana balana = Balana.getInstance();
 	pdpConfig = balana.getPdpConfig();
 	simplePDP = new PDP(pdpConfig);
-	RequestCtx xacmlRequest = buildRequest();
+	RequestCtx xacmlRequest = buildNotApplicableRequest();
 	simplePDP.evaluate(xacmlRequest.toString());
     }
 
     @Test
     public void generatePDPwithSinglePolicy() {
-	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, singlePolicyMultipleRules.getAbsolutePath());
+	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, singlePolicySingleRule.getAbsolutePath());
 	Balana balana = Balana.getInstance();
 	pdpConfig = balana.getPdpConfig();
 	simplePDP = new PDP(pdpConfig);
@@ -63,9 +64,10 @@ public class SimplePDPTest {
 
     @Test
     public void thisRequestWillBeEvaluatedAsNotApplicable() throws URISyntaxException {
+	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, singlePolicySingleRule.getAbsolutePath());
 	pdpConfig = Balana.getInstance().getPdpConfig();
 	simplePDP = new PDP(pdpConfig);
-	RequestCtx request = buildRequest();
+	RequestCtx request = buildNotApplicableRequest();
 	ResponseCtx response = simplePDP.evaluate(request);
 	Set<AbstractResult> results = response.getResults();
 	int expectedSize = 1;
@@ -79,9 +81,21 @@ public class SimplePDPTest {
     }
 
     @Test
-    public void thisRequestWillBeEvaluatedAsPermit() {
+    public void thisRequestWillBeEvaluatedAsPermitAgainstSinglePolicySingleRule() throws URISyntaxException {
+	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, singlePolicySingleRule.getAbsolutePath());
 	pdpConfig = Balana.getInstance().getPdpConfig();
 	simplePDP = new PDP(pdpConfig);
+	RequestCtx request = buildSinglePolicySingleRuleGoodRequest();
+	ResponseCtx response = simplePDP.evaluate(request);
+	Set<AbstractResult> results = response.getResults();
+	int expectedSize = 1;
+	int actualSize = results.size();
+	Assert.assertEquals(expectedSize, actualSize);
+	AbstractResult result = results.iterator().next();
+	log.info("Not applicable request: " + "\n" + result.encode());
+	int expectedDecision = PERMIT;
+	int actualDecision = result.getDecision();
+	Assert.assertEquals(expectedDecision, actualDecision);
     }
 
     @Test
@@ -94,7 +108,19 @@ public class SimplePDPTest {
 
     }
 
-    private RequestCtx buildRequest() throws URISyntaxException {
+    private RequestCtx buildNotApplicableRequest() throws URISyntaxException {
+	Set<Attributes> setOfAttributesSet = new HashSet<>();
+	URI category = new URI("urn:oasis:names:tc:xacml:3.0:attribute-category:resource");
+	Set<Attribute> attributes = new HashSet<>();
+	attributes.add(new Attribute(new URI("attribute.id.1"), "the issuer", new DateTimeAttribute(), new StringAttribute("friends"), 1));
+	Attributes attributeSet = new Attributes(category, attributes);
+	setOfAttributesSet.add(attributeSet);
+	Node documentRoot = null; /** ??? */
+	RequestCtx request = new RequestCtx(setOfAttributesSet, documentRoot);
+	return request;
+    }
+
+    private RequestCtx buildSinglePolicySingleRuleGoodRequest() throws URISyntaxException {
 	Set<Attributes> setOfAttributesSet = new HashSet<>();
 	URI category = new URI("urn:oasis:names:tc:xacml:3.0:attribute-category:resource");
 	Set<Attribute> attributes = new HashSet<>();
