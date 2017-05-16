c8b9bb3d7b48190a40df3012b02f82aa76e7e39d
francesco <francesco.pezzati@iia.cnr.it>
2017-05-16 17:06:41 +0200

    Ok I got a bit more about XACML. Now I want to implement two tests. One should give back PERMIT and one DENY based upon <condition> satisfaction. Let's see.

--- src/test/java/edu/pezzati/sec/xaml/SinglePolicyTest.java
+++ src/test/java/edu/pezzati/sec/xaml/SinglePolicyTest.java
@@ -12,10 +12,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Node;
 import org.wso2.balana.Balana;
-import org.wso2.balana.ConfigurationStore;
 import org.wso2.balana.PDP;
 import org.wso2.balana.PDPConfig;
 import org.wso2.balana.attr.DateTimeAttribute;
+import org.wso2.balana.attr.RFC822NameAttribute;
 import org.wso2.balana.attr.StringAttribute;
 import org.wso2.balana.ctx.AbstractResult;
 import org.wso2.balana.ctx.Attribute;
@@ -24,57 +24,40 @@ import org.wso2.balana.ctx.xacml3.RequestCtx;
 import org.wso2.balana.finder.impl.FileBasedPolicyFinderModule;
 import org.wso2.balana.xacml3.Attributes;
 
-public class SimplePDPTest {
+public class SinglePolicyTest {
 
     private final Logger log = Logger.getLogger(getClass());
     private static final int PERMIT = 0;
-    //    private static final int DENY = 1;
+    private static final int DENY = 1;
     //    private static final int INDETERMINATE = 2;
     private static final int NOT_APPLICABLE = 3;
     private PDPConfig pdpConfig;
     private PDP simplePDP;
-    private File singlePdpConf;
     private File singlePolicySingleRule;
 
     @Before
     public void setUp() {
-	System.clearProperty(ConfigurationStore.PDP_CONFIG_PROPERTY);
-	singlePdpConf = new File("src/test/resources/conf/simple.pdp.conf.xml");
 	System.clearProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY);
 	singlePolicySingleRule = new File("src/test/resources/singlepolicy");
-    }
-
-    public void generatePDPWithNullConfiguration() throws URISyntaxException {
-	System.setProperty(ConfigurationStore.PDP_CONFIG_PROPERTY, singlePdpConf.getAbsolutePath());
-	Balana balana = Balana.getInstance();
-	pdpConfig = balana.getPdpConfig();
-	simplePDP = new PDP(pdpConfig);
-	RequestCtx xacmlRequest = buildNotApplicableRequest();
-	simplePDP.evaluate(xacmlRequest.toString());
+	simplePDP = getPDP(singlePolicySingleRule);
     }
 
     @Test
     public void generatePDPwithSinglePolicy() {
-	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, singlePolicySingleRule.getAbsolutePath());
-	Balana balana = Balana.getInstance();
-	pdpConfig = balana.getPdpConfig();
-	simplePDP = new PDP(pdpConfig);
 	Assert.assertTrue(true);
     }
 
     @Test
     public void thisRequestWillBeEvaluatedAsNotApplicable() throws URISyntaxException {
-	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, singlePolicySingleRule.getAbsolutePath());
-	pdpConfig = Balana.getInstance().getPdpConfig();
-	simplePDP = new PDP(pdpConfig);
 	RequestCtx request = buildNotApplicableRequest();
+	log.info("NOT_APPLICABLE request: " + "\n" + request.toString());
 	ResponseCtx response = simplePDP.evaluate(request);
 	Set<AbstractResult> results = response.getResults();
 	int expectedSize = 1;
 	int actualSize = results.size();
 	Assert.assertEquals(expectedSize, actualSize);
 	AbstractResult result = results.iterator().next();
-	log.info("Not applicable request: " + "\n" + result.encode());
+	log.info("NOT_APPLICABLE result: " + "\n" + result.encode());
 	int expectedDecision = NOT_APPLICABLE;
 	int actualDecision = result.getDecision();
 	Assert.assertEquals(expectedDecision, actualDecision);
@@ -82,30 +65,43 @@ public class SimplePDPTest {
 
     @Test
     public void thisRequestWillBeEvaluatedAsPermitAgainstSinglePolicySingleRule() throws URISyntaxException {
-	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, singlePolicySingleRule.getAbsolutePath());
-	pdpConfig = Balana.getInstance().getPdpConfig();
-	simplePDP = new PDP(pdpConfig);
-	RequestCtx request = buildSinglePolicySingleRuleGoodRequest();
+	RequestCtx request = buildPermitRequest();
+	log.info("PERMIT request: " + "\n" + request.toString());
 	ResponseCtx response = simplePDP.evaluate(request);
 	Set<AbstractResult> results = response.getResults();
 	int expectedSize = 1;
 	int actualSize = results.size();
 	Assert.assertEquals(expectedSize, actualSize);
 	AbstractResult result = results.iterator().next();
-	log.info("Not applicable request: " + "\n" + result.encode());
+	log.info("PERMIT result: " + "\n" + result.encode());
 	int expectedDecision = PERMIT;
 	int actualDecision = result.getDecision();
 	Assert.assertEquals(expectedDecision, actualDecision);
     }
 
     @Test
-    public void thisRequestWillBeEvaluatedAsDeny() {
-
+    public void thisRequestWillBeEvaluatedAsDeny() throws URISyntaxException {
+	RequestCtx request = buildDenyRequest();
+	log.info("DENY request: " + "\n" + request.toString());
+	ResponseCtx response = simplePDP.evaluate(request);
+	Set<AbstractResult> results = response.getResults();
+	AbstractResult result = results.iterator().next();
+	log.info("DENY result: " + "\n" + result.encode());
+	int expectedDecision = DENY;
+	int actualDecision = result.getDecision();
+	Assert.assertEquals(expectedDecision, actualDecision);
     }
 
     @Test
     public void thisRequestWillBeEvaluatedAsIndeterminate() {
 
+    }
+
+    private PDP getPDP(File policy) {
+	System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, policy.getAbsolutePath());
+	Balana balana = Balana.getInstance();
+	pdpConfig = balana.getPdpConfig();
+	return new PDP(pdpConfig);
     }
 
     private RequestCtx buildNotApplicableRequest() throws URISyntaxException {
@@ -115,19 +111,33 @@ public class SimplePDPTest {
 	attributes.add(new Attribute(new URI("attribute.id.1"), "the issuer", new DateTimeAttribute(), new StringAttribute("friends"), 1));
 	Attributes attributeSet = new Attributes(category, attributes);
 	setOfAttributesSet.add(attributeSet);
-	Node documentRoot = null; /** ??? */
+	Node documentRoot = null;
 	RequestCtx request = new RequestCtx(setOfAttributesSet, documentRoot);
 	return request;
     }
 
-    private RequestCtx buildSinglePolicySingleRuleGoodRequest() throws URISyntaxException {
+    private RequestCtx buildPermitRequest() throws URISyntaxException {
 	Set<Attributes> setOfAttributesSet = new HashSet<>();
-	URI category = new URI("urn:oasis:names:tc:xacml:3.0:attribute-category:resource");
+	URI category = new URI("urn:oasis:names:tc:xacml:1.0:subject-category:access-subject");
 	Set<Attribute> attributes = new HashSet<>();
-	attributes.add(new Attribute(new URI("attribute.id.1"), "the issuer", new DateTimeAttribute(), new StringAttribute("friends"), 1));
+	attributes.add(new Attribute(new URI("urn:oasis:names:tc:xacml:1.0:subject:subject-id"), null, new DateTimeAttribute(),
+		new RFC822NameAttribute("jim@med.example.com"), 1));
 	Attributes attributeSet = new Attributes(category, attributes);
 	setOfAttributesSet.add(attributeSet);
-	Node documentRoot = null; /** ??? */
+	Node documentRoot = null;
+	RequestCtx request = new RequestCtx(setOfAttributesSet, documentRoot);
+	return request;
+    }
+
+    private RequestCtx buildDenyRequest() throws URISyntaxException {
+	Set<Attributes> setOfAttributesSet = new HashSet<>();
+	URI category = new URI("urn:oasis:names:tc:xacml:1.0:subject-category:access-subject");
+	Set<Attribute> attributes = new HashSet<>();
+	attributes.add(new Attribute(new URI("urn:oasis:names:tc:xacml:1.0:subject:subject-id"), null, new DateTimeAttribute(),
+		new RFC822NameAttribute("joe@med.example.com"), 1));
+	Attributes attributeSet = new Attributes(category, attributes);
+	setOfAttributesSet.add(attributeSet);
+	Node documentRoot = null;
 	RequestCtx request = new RequestCtx(setOfAttributesSet, documentRoot);
 	return request;
     }
