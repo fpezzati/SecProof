c7fc22b170f51c04661dd45865653e647ea96354
Francesco <francesco.pezzati@iia.cnr.it>
2018-03-15 12:27:32 +0100

    At least a basic working shiro conf.. Now I'll try to add JWT. WIP

--- src/main/java/edu/pezzati/sec/Login.java
+++ src/main/java/edu/pezzati/sec/Login.java
@@ -1,15 +1,24 @@
 package edu.pezzati.sec;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/login")
 public class Login {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
+
     @POST
-    public Response login(String username, String password) {
-	Response resp = null;
-	return resp;
+    public Response login(@FormParam("usrname") String username, @FormParam("passwd") String password) {
+	SecurityUtils.getSubject().login(new UsernamePasswordToken(username, password));
+	SecurityUtils.getSubject().getPrincipals().asList().stream().forEach(principal -> log.info(principal.toString()));
+	return Response.ok("you login succesfully").build();
     }
 }
--- src/main/java/edu/pezzati/sec/token/JWTFilter.java
+++ src/main/java/edu/pezzati/sec/token/JWTFilter.java
@@ -5,22 +5,26 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.web.filter.AccessControlFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JWTFilter extends AccessControlFilter {
 
     private JwtTokenProvider jwtTokenProvider;
+    private Logger log;
 
     public JWTFilter() {
 	jwtTokenProvider = new JwtTokenProvider();
+	log = LoggerFactory.getLogger(getClass());
     }
 
     @Override
     protected boolean isAccessAllowed(ServletRequest req, ServletResponse res, Object arg2) throws Exception {
 	String token = ((HttpServletRequest) req).getHeader("Authorization");
+	log.info("checking {} token.", token);
 	if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
-	    throw new AuthenticationException("Provided token is not a bearer one.");
+	    return false;
 	}
 	token = token.replace("Bearer ", "");
 	jwtTokenProvider.getUser(token);
